c68eb44110a5abc9a85345dac132eded42ad87c8
Joel Maciel <soldado25461@gmail.com>
2021-11-24 14:22:59 -0300

    project created, categories  routes

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+import { categoriesRoutes } from "./routes/Categories.routes";
+
+const app = express();
+app.use(express.json());
+
+app.use(categoriesRoutes);
+
+app.listen(3333, () => {
+  console.log("Server is  running");
+});
