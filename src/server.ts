0dea12429afc5e5eb3a4300bf5cc6dc792462f39
Joel Maciel <soldado25461@gmail.com>
2021-11-25 09:33:13 -0300

    inserindo uuidV4

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
-import { categoriesRoutes } from "./routes/Categories.routes";
+import { categoriesRoutes } from './routes/Categories.routes';
 
 const app = express();
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => {
-  console.log("Server is  running");
+  console.log('Server is  running');
 });
