39b0647cfa8e6055948c009ee8d1b3db7c48892b
Joel Maciel <soldado25461@gmail.com>
2021-12-18 12:06:37 -0300

    criando toker de usuarios

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserCase.ts
@@ -0,0 +1,54 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect');
+    }
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+    const token = sign({}, 'be864703ec03d977d230f5254766cd52', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserCase };
