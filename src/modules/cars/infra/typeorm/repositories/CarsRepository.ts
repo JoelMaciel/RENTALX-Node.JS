1a0e05c332c998f500c57bc5d28264169af75bcc
Joel Maciel <soldado25461@gmail.com>
2022-01-03 12:24:12 -0300

    continuaçao de listagem de carros disponíveis

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -11,6 +11,7 @@ class CarsRepository implements ICarsRepository {
   constructor() {
     this.repository = getRepository(Car);
   }
+
   async create({
     brand,
     category_id,
@@ -38,6 +39,27 @@ class CarsRepository implements ICarsRepository {
       license_plate,
     });
     return car;
+  }
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string,
+  ): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder('c')
+      .where('available = :available', { available: true });
+
+    if (brand) {
+      carsQuery.andWhere('brand = :brand', { brand });
+    }
+    if (name) {
+      carsQuery.andWhere('name = :name', { name });
+    }
+    if (category_id) {
+      carsQuery.andWhere('category_id = :category_id', { category_id });
+    }
+    const cars = await carsQuery.getMany();
+    return cars;
   }
 }
 export { CarsRepository };
