d5cd40528e1502812636c06fc5b78b710746c18a
Joel Maciel <soldado25461@gmail.com>
2022-01-06 14:57:52 -0300

    Finalizando CreateSpecificationUseCase

--- src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -8,7 +8,10 @@ import {
 class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
   specifications: Specification[] = [];
 
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = new Specification();
 
     Object.assign(specification, {
@@ -16,6 +19,7 @@ class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
       name,
     });
     this.specifications.push(specification);
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification> {
--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,3 +1,4 @@
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationRepository';
 import { AppError } from '@shared/errors/AppError';
@@ -12,7 +13,7 @@ class CreateCarSpecificationUseCase {
     private carsRepository: ICarsRepository,
     private specificationsRepository: ISpecificationsRepository,
   ) {}
-  async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
     const carExists = await this.carsRepository.findById(car_id);
 
     if (!carExists) {
@@ -22,8 +23,10 @@ class CreateCarSpecificationUseCase {
       specifications_id,
     );
     carExists.specifications = specifications;
+
     await this.carsRepository.create(carExists);
-    console.log(carExists);
+
+    return carExists;
   }
 }
 export { CreateCarSpecificationUseCase };
