48e1c27725732dbc3cbb50756e63ecfcf71eeb42
Joel Maciel <soldado25461@gmail.com>
2022-01-11 14:26:49 -0300

    criando teste de aluguel

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,7 @@
+import { Rental } from '../infra/typeorm/entities/Rental';
+
+interface IRentalsRepository {
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findRentalByUser(user_id: string): Promise<Rental>;
+}
+export { IRentalsRepository };
